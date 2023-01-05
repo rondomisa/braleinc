c2a9e1b7d0b821962eaa7c5a00c75ca264bd15da
rondomisa <121995396+rondomisa@users.noreply.github.com>
2023-01-05 00:04:08 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav");
+
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav")
